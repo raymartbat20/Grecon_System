73b4db5202504ff1ec2207889fbf960adde01c93
Raymart Palermo <raymartbat20@gmail.com>
2019-08-09 19:42:30 +0800

    first commit

---
+++ resources/views/backend/layouts/scripts/calmScripts.blade.php
@@ -0,0 +1,17 @@
+<!-- plugins:js -->
+  <script src="/__backend/assets/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="/__backend/assets/vendors/chart.js/Chart.min.js"></script>
+  <script src="/__backend/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="/__backend/assets/js/off-canvas.js"></script>
+  <script src="/__backend/assets/js/hoverable-collapse.js"></script>
+  <script src="/__backend/assets/js/template.js"></script>
+  <script src="/__backend/assets/js/settings.js"></script>
+  <script src="/__backend/assets/js/todolist.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="/__backend/assets/js/dashboard.js"></script>
+  <!-- End custom js for this page-->
