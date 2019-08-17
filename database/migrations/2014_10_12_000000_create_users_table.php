d6409615c1c714e5ff676af342d07c2f08aed2ce
raymartbat20 <raymartbat20@gmail.com>
2019-08-17 23:54:51 +0800

    laptop pull

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('user_id');
             $table->string('firstname');
             $table->string('lastname');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('username')->nullable();
             $table->string('password');
             $table->string('role');
