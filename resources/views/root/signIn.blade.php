a6e3004f9d51f64c8df80200888c6917323e67b6
raymartbat20 <raymartbat20@gmail.com>
2019-08-22 18:11:39 +0800

    laptop pull

--- resources/views/root/signIn.blade.php
+++ resources/views/root/signIn.blade.php
@@ -7,7 +7,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>CalmUI Admin</title>
+  <title>Grecon</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="/__backend/assets/vendors/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="/__backend/assets/vendors/css/vendor.bundle.base.css">
