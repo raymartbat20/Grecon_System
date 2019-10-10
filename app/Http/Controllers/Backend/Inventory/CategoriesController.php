2cff07f59b7e2b1d8ead75b5aa70d9b6516a46ac
raymartbat20 <raymartbat20@gmail.com>
2019-10-11 02:53:47 +0800

    capstone update

--- app/Http/Controllers/Backend/Inventory/CategoriesController.php
+++ app/Http/Controllers/Backend/Inventory/CategoriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Backend\Inventory;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Notifications\CategoryResource;
 use App\Category;
 use Hash;
 use DB;
